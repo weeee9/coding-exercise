e24a7d7468d98150acf8321f3b1d33e9aaebc676
weeee9 <teletubby332@gmail.com>
2023-07-23 16:30:36 +0800

    refactor: re-write pascal's traingle in more reuseable way

--- leetcode/medium/118-pascals_triangle/pascals_triangle.go
+++ leetcode/medium/118-pascals_triangle/pascals_triangle.go
@@ -1,21 +1,29 @@
 package pascalstriangle
 
 func generate(numRows int) [][]int {
-	pascalTriangle := make([][]int, 0, numRows)
+	pascalTriangle := make([][]int, numRows)
 
 	for i := 0; i < numRows; i++ {
-		row := make([]int, i+1)
-		for j := 0; j < i+1; j++ {
-			if j == 0 || j == i {
-				row[j] = 1
-			} else if i > 1 {
-				a := pascalTriangle[i-1][j-1]
-				b := pascalTriangle[i-1][j]
-				row[j] = a + b
-			}
+		if i == 0 {
+			pascalTriangle[i] = []int{1}
+		} else {
+			pascalTriangle[i] = generateNextRow(pascalTriangle[i-1])
 		}
-		pascalTriangle = append(pascalTriangle, row)
 	}
 
 	return pascalTriangle
+}
+
+func generateNextRow(prevRow []int) []int {
+	nextRow := make([]int, len(prevRow)+1)
+
+	for i := 0; i < len(nextRow); i++ {
+		if i == 0 || i == len(nextRow)-1 {
+			nextRow[i] = 1
+		} else {
+			nextRow[i] = prevRow[i-1] + prevRow[i]
+		}
+	}
+
+	return nextRow
 }
