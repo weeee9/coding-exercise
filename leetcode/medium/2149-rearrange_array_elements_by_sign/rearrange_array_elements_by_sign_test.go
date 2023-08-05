40596c302d580821be513e14c7a3e231e1e02af4
weeee9 <teletubby332@gmail.com>
2023-08-05 11:56:55 +0800

    fix: remove unnecessary test

--- leetcode/medium/2149-rearrange_array_elements_by_sign/rearrange_array_elements_by_sign_test.go
+++ leetcode/medium/2149-rearrange_array_elements_by_sign/rearrange_array_elements_by_sign_test.go
@@ -45,33 +45,3 @@ func Test_rearrangeArray(t *testing.T) {
 	}
 }
 
-func Test_rotateArray(t *testing.T) {
-	type args struct {
-		nums       []int
-		currentIdx int
-		targetIdx  int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{
-			name: "Test Case 1",
-			args: args{
-				nums:       []int{3, 1, -2, -5, 2, -4},
-				currentIdx: 2,
-				targetIdx:  0,
-			},
-			want: []int{-2, 3, 1, -5, 2, -4},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			rotateArray(tt.args.nums, tt.args.currentIdx, tt.args.targetIdx)
-			if !reflect.DeepEqual(tt.args.nums, tt.want) {
-				t.Errorf("rotateArray() = %v, want %v", tt.args.nums, tt.want)
-			}
-		})
-	}
-}
